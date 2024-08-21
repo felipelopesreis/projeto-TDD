14547425d6dad99cd05a34e019d0fb3e9bb75931
FELIPE LOPES DOS REIS <07041615162@ifms.edu.br>
2024-08-20 20:29:50 -0400

    Teste-aulad-dia20

--- src/test/java/br/edu/ifms/services/TecnicoServiceTests.java
+++ src/test/java/br/edu/ifms/services/TecnicoServiceTests.java
@@ -13,10 +13,12 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import br.edu.ifms.dto.TecnicoDTO;
 import br.edu.ifms.entities.Tecnico;
 import br.edu.ifms.repositories.TecnicoRepository;
 import br.edu.ifms.services.exceptions.DataBaseException;
@@ -62,7 +64,67 @@ public class TecnicoServiceTests {
 		
 		//Salvar dados
 		Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(tecnico);
+		
+		Mockito.when(repository.getReferenceById(idExistente)).thenReturn(tecnico);
+		Mockito.when(repository.getReferenceById(idInexistente)).thenThrow(ResourceNotFoundException.class);
 	}
+	
+	@Test
+	public void updateDeveriaLancarResourceNotFOundExceptionQuandoIdInexistente() {
+		TecnicoDTO dto = Factory.createTecnicoDTO();
+		
+		Assertions.assertThrows(ResourceNotFoundException.class,() ->{
+			service.update(idInexistente, dto);
+		});
+	}
+	
+	@Test
+	public void updateDeveriaAtualizarQuandoIdExistente() {
+		TecnicoDTO dto = Factory.createTecnicoDTO();
+		
+		
+		dto = service.update(idExistente, dto);
+		Assertions.assertNotNull(dto);
+	}		
+	
+	@Test
+	public void insertDeveriaSAlvarQuandoIdNulo() {
+		TecnicoDTO dto = Factory.createTecnicoDTO();
+		dto.setId(null);
+
+		dto = service.insert(dto);
+		Assertions.assertNotNull(dto);
+		
+	}
+	
+	
+	@Test
+	public void findByIdDeveriaLancarResourceNotFoundExceptionQuandoIdInexistente() {
+		Assertions.assertThrows(ResourceNotFoundException.class,() ->{
+			service.findById(idInexistente);
+		});
+		
+	}
+	
+	
+
+	@Test
+	public void findByIdDeveriaRetornarObjetoQuandoIdExistente() {
+		TecnicoDTO dto = service.findById(idExistente);
+		Assertions.assertNotNull(dto);
+		
+	}
+	
+	
+	
+	@Test
+	public void findAllPagedDeveriaRetornarPagina() {
+		Pageable pageable = Pageable.ofSize(10);
+		Page<TecnicoDTO> pagina = service.findAllPaged(pageable);
+		Assertions.assertNotNull(pagina);
+		
+	}
+	
 	
 	@Test
 	public void deleteDeveriaLancarDataBaseExceptionQuandoIdDependente() {
